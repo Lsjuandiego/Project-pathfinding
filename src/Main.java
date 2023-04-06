351398c5c97262f217e10909963c72083150d336
Juan Lopez <lsjuandiegoo@gmail.com>
2023-04-06 15:54:46 -0500

    algoritmo BFS

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         GraphReader reader = new GraphReader();
         Graph graph = reader.readGraphFromFile("src/Files/camino.txt");
 
+        Node start = null;
+        Node goal = null;
+
+        /*
+        DFS
+         */
+        for (Node node : graph.getNodes()) {
+            if (node.isStart()) {
+                start = node;
+            } else if (node.isGoal()) {
+                goal = node;
+            }
+        }
+
+        List<Node> path = BFS.search(graph, start, goal);
+
+        if (path != null) {
+            System.out.println("Path found:");
+            for (Node node : path) {
+                System.out.println(node.getValue() + " at (" + node.getRow() + ", " + node.getCol() + ")");
+            }
+        } else {
+            System.out.println("No path found.");
+        }
+
+        System.out.println("Visited nodes:");
+        for (Node node : BFS.visitedNodes) {
+            System.out.println(node.getValue() + " at (" + node.getRow() + ", " + node.getCol() + ")");
+        }
+        /*
+        termina DFS
+         */
+
+        /*
         for (Node node : graph.getNodes()) {
             System.out.println("Node: " + node.getValue() + " (Row: " + node.getRow() + ", Col: " + node.getCol() + ")");
             System.out.print("Neighbors: ");
@@ -11,5 +47,7 @@ public class Main {
             }
             System.out.println();
         }
+
+         */
     }
 }

29cd566af7fd9c2440e8cb501b96685160f72856
Juan Lopez <129210826+SIngBio@users.noreply.github.com>
2023-04-15 15:11:30 -0500

    se agregó el metodo para calcular la anchura del mapa y la altura

--- src/Graph.java
+++ src/Graph.java
@@ -18,7 +18,26 @@ class Graph {
         return nodes;
     }
 
+    public int getHeight() {
+        int numNodes = nodes.size();
+        int numColumns = getColumnWidth();
+        return (int) Math.floor(numNodes / numColumns);
+    }
 
+    public int getColumnWidth() {
+        if (getNodes().isEmpty()) {
+            return 0;
+        }
+        // Column width is the number of nodes in the first row
+        int firstRow = getNodes().get(0).getRow();
+        int count = 0;
+        for (Node node : getNodes()) {
+            if (node.getRow() == firstRow) {
+                count++;
+            }
+        }
+        return count;
+    }
     public Node getStartNode() {
         for (Node node : nodes) {
             if (node.isStart()) {
